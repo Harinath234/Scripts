d5ab9c8894223a77066da26c495fee952e61fc56
Harinath234 <127993674+Harinath234@users.noreply.github.com>
2026-02-28 01:12:11 +0530

    Fix typo in yum update command

--- Jenkins_Setup_Script.sh
+++ Jenkins_Setup_Script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum update –y
+sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/rpm-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/rpm-stable/jenkins.io-2026.key
 sudo yum upgrade
