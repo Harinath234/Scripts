e9bdbe142c896a5065c0609dcb9e408c182e8ccb
Harinath <127993674+Harinath234@users.noreply.github.com>
2026-03-11 23:41:46 +0530

    Update Tomcat_Setup_Script_PortChange.sh

--- Tomcat_Setup_Script_PortChange.sh
+++ Tomcat_Setup_Script_PortChange.sh
@@ -2,6 +2,7 @@
 
 sudo yum install java -y
 cd /opt
+
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.115/bin/apache-tomcat-9.0.115.tar.gz
 sudo tar -xvf apache-tomcat-9.0.115.tar.gz
 
@@ -10,6 +11,7 @@ sudo sed -i 's/port="8080"/port="8081"/g' /opt/apache-tomcat-9.0.115/conf/server
 
 cd /opt/apache-tomcat-9.0.115/webapps/manager/META-INF
 sudo sed -i '/CookieProcessor/,/HashMap"\/>/d' context.xml
+
 cd /opt/apache-tomcat-9.0.115/conf
 sudo mv tomcat-users.xml tomcat-users_bkup.xml
 sudo bash -c 'cat > tomcat-users.xml <<EOF
